9e69a5ed103db10920d75b00180ccda86a795a4a
Naohiro IWAMOTO <naodai24@gmail.com>
2018-01-28 19:45:34 +0900

    add postcss-custom-media

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ module.exports = (ctx) => ({
   map: ctx.options.map,
   plugins: [
 		require('postcss-import'),
+    require('postcss-custom-media'),
     require('postcss-nested'),
     require('autoprefixer'),
   ],
