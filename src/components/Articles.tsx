f51a64ccb2938eae4ba624488640512b35446401
Naohiro Iwamoto <naodai24@gmail.com>
2018-06-21 00:05:22 +0900

    add data.json

--- src/components/Articles.tsx
+++ src/components/Articles.tsx
@@ -12,43 +12,79 @@ interface Item {
   link: string;
 }
 
-const Articles = () => {
-  const prefix = "st-Grid";
-  const labels: Items[] = [
-    {
-      name: "Project",
-      items: [
-        {
-          title: "ゆゆ式",
-          info: "github.com/mayok/yuyushiki",
-          link: "https://github.com/mayok/yuyushiki"
-        }
-      ]
-    },
-    {
-      name: "hoge",
-      items: [
-        {
-          title: "hoge",
-          info: "",
-          link: ""
-        }
-      ]
-    }
-  ];
-  return (
-    <div className={`${prefix}_Wrapper`}>
-      {labels.map((label: Items) => (
-        <div className={prefix}>
-          <h2 className={`${prefix}_MajorTitle`}>{label.name}</h2>
+interface State {
+  [key: string]: Item[];
+}
 
-          {label.items.map((item: Item) => (
-            <Article title={item.title} info={item.info} link={item.link} />
-          ))}
-        </div>
-      ))}
-    </div>
-  );
-};
+class Articles extends React.Component<{}, State> {
+  constructor(props: any) {
+    super(props);
+
+    this.setState({});
+  }
+
+  componentDidMount() {
+    fetch("/data.json")
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          [data.data[0].name]: data.data[0].items,
+          [data.data[1].name]: data.data[1].items
+        });
+      });
+  }
+
+  render() {
+    const prefix = "st-Grid";
+    const labels: Items[] = [
+      {
+        name: "Project",
+        items: [
+          {
+            title: "ゆゆ式",
+            info: "github.com/mayok/yuyushiki",
+            link: "https://github.com/mayok/yuyushiki"
+          }
+        ]
+      },
+      {
+        name: "hoge",
+        items: [
+          {
+            title: "hoge",
+            info: "",
+            link: ""
+          }
+        ]
+      }
+    ];
+    return (
+      <div className={`${prefix}_Wrapper`}>
+        {this.state["project"].map((item: Item) => (
+          <Article
+            key={item.title}
+            title={item.title}
+            info={item.info}
+            link={item.link}
+          />
+        ))}
+        {/* {labels.map((label: Items) => (
+          <div key={label.name.toLowerCase()} className={prefix}>
+            <h2 className={`${prefix}_MajorTitle`}>{label.name}</h2>
+
+            {label.items.map((item: Item) => (
+              <Article
+                key={item.title}
+                title={item.title}
+                info={item.info}
+                link={item.link}
+              />
+            ))}
+          </div>
+        ))} */}
+      </div>
+    );
+  }
+}
 
 export default Articles;
